ce7f12e84f6593610b3112909037ac1cda51a6c0
reinasroom <non>
2024-05-08 22:11:50 +0900

    change title

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import '../style/globals.css';
 export default function Home() {
   return (
     <div>
+      <title>正規表現チェッカー</title>
       <h1 className="text-4xl font-extrabold">正規表現チェッカー</h1>
       <RegexCheckerForm />
     </div>
